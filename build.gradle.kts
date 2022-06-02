1f49f36c0496c3e7544d2fba9d13e9990f96cff5
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2022-06-02 02:29:40 +0200

    again 6 total new

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,8 @@ version = MetaProject.version
 plugins {
     id("org.jetbrains.dokka") version MetaProject.dokkaVersion
     id("org.jetbrains.kotlinx.kover") version MetaProject.koverVersion
-    id("com.github.kt3k.coveralls") version "2.12.0"
+    id("com.github.nbaztec.coveralls-jacoco") version "1.2.14"
+    //id("com.github.kt3k.coveralls") version "2.12.0"
     id("project-publish-setup")
 }
 
@@ -43,7 +44,7 @@ tasks.dokkaHtmlMultiModule.configure {
     outputDirectory.set(buildDir.resolve("dokkaCustomMultiModuleOutput"))
 }
 
-coveralls {
+/*coveralls {
     jacocoReportPath = "buffer/build/reports/kover/project-xml/report.xml"
     sendToCoveralls = true
     sourceDirs = listOf(
@@ -52,4 +53,19 @@ coveralls {
         "$buildDir/buffer/src/jsMain/kotlin/",
         "$buildDir/buffer/src/nativeMain/kotlin/",
     )
+}*/
+
+coverallsJacoco {
+    reportPath = "$buildDir/buffer/build/reports/kover/project-xml/report.xml"
+
+    reportSourceSets = listOf(
+        File("$buildDir/buffer/src/commonMain/kotlin/"),
+        File("$buildDir/buffer/src/jvmMain/kotlin/"),
+        File("$buildDir/buffer/src/jsMain/kotlin/"),
+        File("$buildDir/buffer/src/nativeMain/kotlin/"),
+    )
+    //apiEndpoint = "" // default: https://coveralls.io/api/v1/jobs
+
+    //dryRun = false // default: false
+    //coverallsRequest = File("build/req.json") // default: null
 }
