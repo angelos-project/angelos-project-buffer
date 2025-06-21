f19a6e5207ebbe2a2a07851062fb4dc493142ee9
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-06-21 22:17:38 +0200

    In the middle of segments

--- library/src/commonMain/kotlin/org/angproj/io/buf/seg/ByteString.kt
+++ library/src/commonMain/kotlin/org/angproj/io/buf/seg/ByteString.kt
@@ -19,37 +19,48 @@ import org.angproj.io.buf.WriteAccess
 import org.angproj.io.buf.util.Limitable
 
 
-public abstract class ByteString: Limitable, ReadAccess, WriteAccess {
+/**
+ * A [ByteString] is a string of bytes that can be read and written to.
+ * It has a fixed size and a limit that can be set.
+ *
+ * @property stringSize The size of the segment in bytes.
+ * @property stringLimit The limit of the segment in bytes, defaults to [stringSize].
+ */
+public abstract class ByteString(
+    protected val stringSize: Int,
+    protected var stringLimit: Int = stringSize
+): Limitable, ReadAccess, WriteAccess {
 
     protected inline fun <reified R: Any> Int.checkRangeByte(): Unit = when(this) {
-        !in 0..<limit -> throw IllegalArgumentException("Out of bounds. Byte - $this")
+        !in 0..<stringLimit -> throw IllegalArgumentException("Out of bounds. Byte - $this")
         else -> Unit
     }
 
     protected inline fun <reified R: Any> Int.checkRangeShort(): Unit = when(this) {
-        !in 0..<limit-1 -> throw IllegalArgumentException("Out of bounds. Short - $this")
+        !in 0..<stringLimit-1 -> throw IllegalArgumentException("Out of bounds. Short - $this")
         else -> Unit
     }
 
     protected inline fun <reified R: Any> Int.checkRangeInt(): Unit = when(this) {
-        !in 0..<limit-3 -> throw IllegalArgumentException("Out of bounds. Int - $this")
+        !in 0..<stringLimit-3 -> throw IllegalArgumentException("Out of bounds. Int - $this")
         else -> Unit
     }
 
     protected inline fun <reified R: Any> Int.checkRangeLong(): Unit = when(this) {
-        !in 0..<limit-7 -> throw IllegalArgumentException("Out of bounds. Long - $this")
+        !in 0..<stringLimit-7 -> throw IllegalArgumentException("Out of bounds. Long - $this")
         else -> Unit
     }
 
     override val limit: Int
-        get() = TODO("Not yet implemented")
+        get() = stringLimit
 
     override fun limitAt(newLimit: Int) {
-        TODO("Not yet implemented")
+        require(newLimit in 0..size) { "New limit must be between 0 and $stringSize" }
+        stringLimit = newLimit
     }
 
     override val size: Int
-        get() = TODO("Not yet implemented")
+        get() = stringSize
 
     abstract override fun getByte(index: Int): Byte
 
--- library/src/commonMain/kotlin/org/angproj/io/buf/seg/Model.kt
+++ library/src/commonMain/kotlin/org/angproj/io/buf/seg/Model.kt
@@ -1,4 +1,65 @@
+/**
+ * Copyright (c) 2024 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
 package org.angproj.io.buf.seg
 
-public class Model {
+public class Model : Segment<Model>() {
+    override fun getByte(index: Int): Byte {
+        index.checkRangeByte<Unit>()
+        // Implementation here
+        TODO("Not yet implemented")
+    }
+
+    override fun getShort(index: Int): Short {
+        index.checkRangeShort<Unit>()
+        // Implementation here
+        TODO("Not yet implemented")
+    }
+
+    override fun getInt(index: Int): Int {
+        index.checkRangeInt<Unit>()
+        // Implementation here
+        TODO("Not yet implemented")
+    }
+
+    override fun getLong(index: Int): Long {
+        index.checkRangeLong<Unit>()
+        // Implementation here
+        TODO("Not yet implemented")
+    }
+
+    override fun setByte(index: Int, value: Byte) {
+        index.checkRangeByte<Unit>()
+        // Implementation here
+        TODO("Not yet implemented")
+    }
+
+    override fun setShort(index: Int, value: Short) {
+        index.checkRangeShort<Unit>()
+        // Implementation here
+        TODO("Not yet implemented")
+    }
+
+    override fun setInt(index: Int, value: Int) {
+        index.checkRangeInt<Unit>()
+        // Implementation here
+        TODO("Not yet implemented")
+    }
+
+    override fun setLong(index: Int, value: Long) {
+        index.checkRangeLong<Unit>()
+        // Implementation here
+        TODO("Not yet implemented")
+    }
 }
