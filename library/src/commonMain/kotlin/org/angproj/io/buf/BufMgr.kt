333b37e759447b6f560949456f71a03a6e8b0136
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-06-23 14:09:22 +0200

    Added array buffers creation

--- library/src/commonMain/kotlin/org/angproj/io/buf/BufMgr.kt
+++ library/src/commonMain/kotlin/org/angproj/io/buf/BufMgr.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2024 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ * Copyright (c) 2024-2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
  *
  * This software is available under the terms of the MIT license. Parts are licensed
  * under different terms if stated. The legal terms are attached to the LICENSE file
@@ -14,6 +14,27 @@
  */
 package org.angproj.io.buf
 
+import org.angproj.io.buf.mem.Default
+import org.angproj.sec.util.TypeSize
+
 public object BufMgr {
-    public fun byteBuffer(size: Int): ByteBuffer = ByteBuffer(Default.allocate(size), false)
+    public fun byteBuf(size: Int): ByteBuffer = ByteBuffer(Default.allocate(size), false)
+
+    public fun uByteBuf(size: Int): UByteBuffer = UByteBuffer(Default.allocate(size), false)
+
+    public fun shortBuf(size: Int): ShortBuffer = ShortBuffer(Default.allocate(size * TypeSize.shortSize), false)
+
+    public fun uShortBuf(size: Int): UShortBuffer = UShortBuffer(Default.allocate(size * TypeSize.uShortSize), false)
+
+    public fun intBuf(size: Int): IntBuffer = IntBuffer(Default.allocate(size * TypeSize.intSize), false)
+
+    public fun uIntBuf(size: Int): UIntBuffer = UIntBuffer(Default.allocate(size * TypeSize.uIntSize), false)
+
+    public fun longBuf(size: Int): LongBuffer = LongBuffer(Default.allocate(size * TypeSize.longSize), false)
+
+    public fun uLongBuf(size: Int): ULongBuffer = ULongBuffer(Default.allocate(size * TypeSize.uLongSize), false)
+
+    public fun floatBuf(size: Int): FloatBuffer = FloatBuffer(Default.allocate(size * TypeSize.floatSize), false)
+
+    public fun doubleBuf(size: Int): DoubleBuffer = DoubleBuffer(Default.allocate(size * TypeSize.doubleSize), false)
 }
--- library/src/commonMain/kotlin/org/angproj/io/buf/ShortBuffer.kt
+++ library/src/commonMain/kotlin/org/angproj/io/buf/ShortBuffer.kt
@@ -18,7 +18,7 @@ import org.angproj.io.buf.seg.Segment
 import org.angproj.sec.util.TypeSize
 
 
-public class ShortBuffer protected constructor(
+public class ShortBuffer internal constructor(
     segment: Segment<*>, view: Boolean = false
 ): ArrayBuffer<Short>(segment, view, TypeSize.shortSize) {
 
