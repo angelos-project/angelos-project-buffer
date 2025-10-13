58475177164df2bb4a33af07ebf407084fd0b23b
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-10-13 16:44:32 +0200

    Adding notice to tests

--- library/src/commonTest/kotlin/org/angproj/io/buf/mem/FixedBytesPoolTest.kt
+++ library/src/commonTest/kotlin/org/angproj/io/buf/mem/FixedBytesPoolTest.kt
@@ -1,10 +1,23 @@
+/**
+ * Copyright (c) 2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
 package org.angproj.io.buf.mem
 
 
 import org.angproj.io.buf.util.DataSize
 import org.angproj.io.buf.seg.Bytes
 import kotlin.test.Test
-import kotlin.test.assertFailsWith
 
 class FixedBytesPoolTest : AbstractFixedPoolTest<Bytes, FixedBytesPool>() {
 
