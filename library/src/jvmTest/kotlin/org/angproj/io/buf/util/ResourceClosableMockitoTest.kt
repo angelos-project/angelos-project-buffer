6f05bc6f4e871ab38f6a7d43ee8ddcbbc7327c22
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-10-24 22:42:49 +0200

    Mocking ResourceClosable

---
+++ library/src/jvmTest/kotlin/org/angproj/io/buf/util/ResourceClosableMockitoTest.kt
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.angproj.io.buf.util
+
+import org.mockito.Mock
+import org.mockito.kotlin.doNothing
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import kotlin.test.Test
+import kotlin.test.BeforeTest
+import kotlin.test.assertEquals
+
+class ResourceClosableMockitoTest {
+
+    @Mock
+    private lateinit var closeable: ResourceClosable
+
+    @BeforeTest
+    fun setup() {
+        closeable = mock()
+    }
+
+    @Test
+    fun testClose() {
+        doNothing().whenever(closeable).close()
+        assertEquals(Unit, closeable.close())
+    }
+
+    @Test
+    fun testIsOpenTrue() {
+        whenever(closeable.isOpen()).thenReturn(true)
+        assertEquals(true, closeable.isOpen())
+    }
+
+    @Test
+    fun testIsOpenFalse() {
+        whenever(closeable.isOpen()).thenReturn(false)
+        assertEquals(false, closeable.isOpen())
+    }
+}
